a7b647fca6b8058a429f7291265f565a5f823270
Jochem Loedeman <60843521+jochemloedeman@users.noreply.github.com>
2026-03-04 19:15:50 +0100

    restructure

--- monitor/postgres/postgres.go
+++ monitor/postgres/postgres.go
@@ -2,7 +2,10 @@ package postgres
 
 import (
 	"context"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jochemloedeman/misty/monitor"
 	"github.com/jochemloedeman/misty/monitor/postgres/sqlc"
@@ -28,4 +31,15 @@ func NewRunAtomically(pool *pgxpool.Pool) monitor.RunAtomically {
 		}
 		return tx.Commit(ctx)
 	}
+}
+
+func dbTime(ts time.Time) pgtype.Timestamptz {
+	if ts.IsZero() {
+		return pgtype.Timestamptz{}
+	}
+	return pgtype.Timestamptz{Time: ts, Valid: true}
+}
+
+func dbUUID(id uuid.UUID) pgtype.UUID {
+	return pgtype.UUID{Bytes: id, Valid: true}
 }
