37b744cfc0cf8b1d282225ef1646da1ce568ef9b
Jochem Loedeman <60843521+jochemloedeman@users.noreply.github.com>
2026-03-22 20:18:40 +0100

    dev compose

--- caddy/entrypoint.sh
+++ caddy/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-export CLOUDFLARE_API_TOKEN=$(cat /run/secrets/cloudflare_api_token)
+if [ -f /run/secrets/cloudflare_api_token ]; then
+    export CLOUDFLARE_API_TOKEN=$(cat /run/secrets/cloudflare_api_token)
+fi
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
