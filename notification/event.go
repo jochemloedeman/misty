f0ab4bc327968c22244a61113f1ff8631abe923c
Jochem Loedeman <60843521+jochemloedeman@users.noreply.github.com>
2026-06-16 23:05:18 +0200

    queue

--- notification/event.go
+++ notification/event.go
@@ -2,8 +2,6 @@ package notification
 
 import "github.com/google/uuid"
 
-// Queued reports that a notification was persisted to the outbox and is ready
-// for delivery.
 type Queued struct {
 	ID uuid.UUID
 }
